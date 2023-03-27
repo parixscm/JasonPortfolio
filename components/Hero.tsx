8711b3df72d12ff8f429ff257fdf684736180c3f
parixscm <parixscm@gmail.com>
2023-03-28 01:02:03 +0900

    📝 Feat: ADD hero section with type animation

---
+++ components/Hero.tsx
@@ -0,0 +1,26 @@
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+
+type Props = {};
+
+function Hero({}: Props) {
+  const [text] = useTypewriter({
+    words: [
+      "안녕하세요! 제이슨 입니다!",
+      "여행을-많이-좋아합니다.tsx",
+      "<하지만코딩이더재밌는걸요? />",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
+  return (
+    <div>
+      <h1>
+        <span className="mr-2">{text}</span>
+        <Cursor cursorColor="#F7AB0A" />
+      </h1>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
 import Head from "next/head";
 import Header from "../../components/Header";
+import Hero from "../../components/Hero";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-screen bg-[rgb(36,36,36)] text-white">
       <Head>
         <title>제이슨 포트</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
-    </>
+      <section id="hero">
+        <Hero />
+      </section>
+    </div>
   );
 }
