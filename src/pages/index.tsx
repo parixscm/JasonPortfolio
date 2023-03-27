02ef3fee87f740528c12610a996ecdcb4ac24966
parixscm <parixscm@gmail.com>
2023-03-28 00:05:30 +0900

    🎉 Feat: ADD initial portfolio files

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>제이슨 포트</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="text-red-400">포트폴리오 제작</div>
+    </>
+  );
+}
