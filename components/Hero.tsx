6ce5632569bb23b4738a5ba865f5cc363a41012f
parixscm <parixscm@gmail.com>
2023-03-28 02:26:38 +0900

    💄 Design: ADD catch phrase to hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
 import BgCircles from "./BgCircles";
 
@@ -17,10 +18,31 @@ function Hero({}: Props) {
   return (
     <div className="flex h-screen flex-col items-center justify-center space-y-8 overflow-hidden text-center">
       <BgCircles />
-      <h1>
-        <span className="mr-2">{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      {/* TODO: 이미지 태그로 교체 (object-cover 추가) */}
+      <div className="h-32 w-32 rounded-full bg-blue-400" />
+      <div className="z-20">
+        <h2 className="pb-2 text-sm uppercase tracking-[15px] text-gray-500">
+          front-end developer
+        </h2>
+        <h1 className="px-10 text-5xl font-semibold transition-all duration-500 ease-in-out xl:text-6xl">
+          <span className="mr-2">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroBtn">about</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroBtn">experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroBtn">skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroBtn">projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
