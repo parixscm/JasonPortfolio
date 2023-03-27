eec41c23c43cf39f35d16e4b279a80f23bbbbfdf
parixscm <parixscm@gmail.com>
2023-03-28 02:29:00 +0900

    🚑 Design: FIX overflow bug in hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,7 @@ function Hero({}: Props) {
   });
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-8 overflow-hidden text-center">
+    <div className="flex h-screen flex-col items-center justify-center space-y-8  text-center">
       <BgCircles />
       {/* TODO: 이미지 태그로 교체 (object-cover 추가) */}
       <div className="h-32 w-32 rounded-full bg-blue-400" />
