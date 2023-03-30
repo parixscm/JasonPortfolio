64e5ea66269320993f2c004f06ed74464da1983e
parixscm <parixscm@gmail.com>
2023-03-30 13:24:57 +0900

    🚑 Fix: FIX fetch utility functions

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -1,11 +1,11 @@
-import { Experience } from "../typings";
+import { Exp } from "../typings";
 
 export const fetchExperiences = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`
+    `${process.env.NEXT_PUBLIC_SANITY_BASE_URL}/api/getExperiences`
   );
   const data = await res.json();
-  const experiences: Experience[] = data.experiences;
+  const experiences: Exp[] = data.experiences;
 
   return experiences;
 };
